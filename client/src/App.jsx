60cf4a8d0f8da7defcd7241a32b04c1772e5dd16
Ahmad Shahzad <121196369+AhmadShahzad0701@users.noreply.github.com>
2025-06-19 13:39:29 +0500

    Resolve import issue
    inside the sections folder, We need to import them individually

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,9 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import {Hero, Footer} from "./sections"
+import Hero from './sections/Hero';
+import Footer from './sections/Footer';
+
 
 function App() {
   const [count, setCount] = useState(0)
