50b08cc94842ceb028e97f521e3c47d7ea555bd8
Ahmad Shahzad <121196369+AhmadShahzad0701@users.noreply.github.com>
2025-06-19 13:35:42 +0500

    App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,17 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Hero, Footer} from "./sections"
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div className='flex'>
-      <h1 className='text-3xl font-bold bg-red-500'>Hello</h1>
-      <h1 className='text-xl'>Ahmad</h1>
-    </div>
-
+    <main>
+      <section><Hero/></section>
+      <section><Footer/></section>
+    </main>
     </>
   )
 }
